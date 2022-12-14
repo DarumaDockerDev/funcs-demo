3eb330862eb78609696137bb65cbd83625aef656
DarumaDocker <DarumaDocker@gmail.com>
2022-12-14 22:48:35 +0800

    init

---
+++ slack/slack/normal/src/lib.rs
@@ -0,0 +1,15 @@
+use slack_flows::{get_event, message_from_channel, send_message_to_channel};
+
+#[no_mangle]
+pub fn prepare() {
+    if let Some(sm) = message_from_channel("reactorlocal", "random") {
+        send_message_to_channel("reactorlocal", "general", sm.text);
+    }
+}
+
+#[no_mangle]
+pub fn work() {
+    if let Some(sm) = get_event() {
+        send_message_to_channel("reactorlocal", "random", sm.text + "---");
+    }
+}
