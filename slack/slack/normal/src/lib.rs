729f83e43aec2e28b0c2f3ecd325b5accf86aef2
DarumaDocker <DarumaDocker@gmail.com>
2022-12-15 23:23:21 +0800

    update

--- slack/slack/normal/src/lib.rs
+++ slack/slack/normal/src/lib.rs
@@ -8,11 +8,15 @@ pub fn prepare() {
     if let Some(sm) = listen_to_channel("reactorlocal", "t1") {
         send_message_to_channel("reactorlocal", "t1", sm.text);
     }
+    if let Some(sm) = listen_to_channel("reactorlocal", "random") {
+        send_message_to_channel("reactorlocal", "t1", sm.text);
+    }
 }
 
 #[no_mangle]
 pub fn work() {
     if let Some(sm) = message_from_channel() {
-        send_message_to_channel("reactorlocal", "t1", sm.text + "---");
+        send_message_to_channel("reactorlocal", "general", sm.text + "---");
+        send_message_to_channel("reactorlocal", "t1", String::from("new message"));
     }
 }
