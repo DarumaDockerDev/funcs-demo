1ef69f2c1b09cff761b49fc9585b74fe145683e8
DarumaDocker <DarumaDocker@gmail.com>
2023-01-03 19:36:15 +0800

    add store support

--- lambda/normal/src/lib.rs
+++ lambda/normal/src/lib.rs
@@ -1,5 +1,6 @@
 use lambda_flows::{listen_to_request, message_from_request, send_response};
 use slack_flows::send_message_to_channel;
+use store_flows::{get, set};
 
 #[no_mangle]
 pub fn register() {
@@ -8,12 +9,20 @@ pub fn register() {
 
 #[no_mangle]
 pub fn work() {
-    let (qry, body) = message_from_request();
-    send_message_to_channel(
-        "reactorlocal",
-        "random",
-        String::from_utf8_lossy(&body).into_owned(),
-    );
+    let (_qry, body) = message_from_request();
+    let count = match get("count") {
+        Some(c) => c.as_i64().unwrap_or(0) + 1,
+        None => 1,
+    };
+    set("count", serde_json::json!(count));
+
+    if count % 2 == 0 {
+        send_message_to_channel(
+            "reactorlocal",
+            "random",
+            String::from_utf8_lossy(&body).into_owned(),
+        );
+    }
     let v = vec![1; 10000];
     send_response(v);
 }
