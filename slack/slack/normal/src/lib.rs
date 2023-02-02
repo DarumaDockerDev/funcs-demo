302dd253e35b6f980b9e6b66da10ca97643a3f35
DarumaDocker <DarumaDocker@gmail.com>
2023-02-02 16:39:57 +0800

    send tweet while receiving slack

--- slack/slack/normal/src/lib.rs
+++ slack/slack/normal/src/lib.rs
@@ -1,9 +1,13 @@
 use slack_flows::{listen_to_channel, send_message_to_channel};
+use twitter_flows::create_tweet;
 
 #[no_mangle]
 pub fn run() {
     listen_to_channel("reactorlocal", "t1", |sm| {
         send_message_to_channel("reactorlocal", "general", format!("Hello, {}", sm.text));
-        send_message_to_channel("reactorlocal", "t1", String::from("new message"));
+        let tweet = serde_json::json!({
+            "text": sm.text
+        });
+        create_tweet(tweet);
     });
 }
