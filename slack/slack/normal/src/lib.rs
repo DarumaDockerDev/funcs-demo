18b9028336b487bb8bd95b862d2c6ed8c4033105
DarumaDocker <DarumaDocker@gmail.com>
2023-02-08 11:50:34 +0800

    for log test

--- slack/slack/normal/src/lib.rs
+++ slack/slack/normal/src/lib.rs
@@ -3,11 +3,13 @@ use twitter_flows::create_tweet;
 
 #[no_mangle]
 pub fn run() {
-    listen_to_channel("reactorlocal", "t1", |sm| {
+    listen_to_channel("reactorlocal-", "t1", |sm| {
         send_message_to_channel("reactorlocal", "general", format!("Hello, {}", sm.text));
+        /*
         let tweet = serde_json::json!({
             "text": sm.text
         });
         create_tweet(tweet);
+        */
     });
 }
