a46d04e40115e6a325e1aaa2c711a3939778147b
DarumaDocker <DarumaDocker@gmail.com>
2023-02-12 20:34:01 +0800

    test for openai

--- slack/slack/normal/src/lib.rs
+++ slack/slack/normal/src/lib.rs
@@ -1,10 +1,18 @@
+use openai_flows::{create_completion, CompletionRequest};
 use slack_flows::{listen_to_channel, send_message_to_channel};
-use twitter_flows::create_tweet;
+// use twitter_flows::create_tweet;
 
 #[no_mangle]
 pub fn run() {
     listen_to_channel("reactorlocal", "t1", |sm| {
-        send_message_to_channel("reactorlocal", "general", format!("Hello, {}", sm.text));
+        let cr = CompletionRequest {
+            prompt: sm.text,
+            ..Default::default()
+        };
+        let r = create_completion("DarumaDocker", cr);
+        r.iter().for_each(|c| {
+            send_message_to_channel("reactorlocal", "general", c.to_string());
+        });
         /*
         let tweet = serde_json::json!({
             "text": sm.text
